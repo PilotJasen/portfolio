2b2c2ef69904fd10a3c50afe89cb5dc6c6bc1f08
PilotJasen <103771337+PilotJasen@users.noreply.github.com>
2024-02-10 13:58:34 -0800

    Added comments to the "gettemp.sh".

--- raspberry/gettemp.sh
+++ raspberry/gettemp.sh
@@ -4,22 +4,16 @@
 # NAME: Get Temperature  #
 # AUTHOR: DesertRatz     #
 # CREATED: 2022/08/27    #
-# (C) 2022-2023          #
+# (C) 2022-2024          #
 ##########################
 
-#This function will call the raspberry pi "vcgencmd measure_temp".
+# We will run the RPi vcgen to obtain the temperatures. (JEA_20240210)
 cpu=$(/usr/bin/vcgencmd measure_temp | awk -F "[=\']" '{print $2}')
 
-#gpu=$(/usr/bin/vcgencmd measure_temp | awk -F "[=\']" '{print $2}')
-
-#cpu=$(</sys/class/thermal/thermal_zone0/temp)
-
-#cpu=$(echo "$cpu / 100 * 0.1" | bc)
+# We will convert the temp "C" to temp "F". (JEA_20240210)
 cpuf=$(echo "(1.8 * $cpu) + 32" | bc)
 
-#gpuf=$(echo "(1.8 * $gpu) + 32" |bc)
-
+# We will format the output here. (JEA_20240210)
 echo "$(date) @ $(hostname)"
-echo "-------------------------------------------"
-#echo "GPU => $gpu'C ($gpuf'F)"
+echo "----------------------------"
 echo "CPU => $cpu'C ($cpuf'F)"
